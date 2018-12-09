0bee66e0d2c3054c9bb89d32a6b3b82e8f147198
OnclePhil19 <info@escolafrancobrasileira.com.br>
2018-12-09 17:15:56 -0200

    probleme login

--- index.php
+++ index.php
@@ -1,54 +1,23 @@
 <?php 
 session_start();
-
 require_once("vendor/autoload.php");
-
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
-use \Hcode\Model\user;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
-
 $app->config('debug', true);
 
-$app->get('/', function() {
-	$page = new Page();
+require_once("functions.php");
+require_once("site.php");
+require_once("admin.php");
+require_once("admin-users.php");
+require_once("admin-categories.php");
+require_once("admin-products.php");
 
-	$page->setTpl("index");
-   
-});
-
-$app->get('/admin', function() {
-	User::verifyLogin();
-	$page = new PageAdmin();
-
-	$page->setTpl("index");
-   
-});
-
-$app->get('/admin/login', function() {
-
-	$page = new PageAdmin([
-		"header"=>false,
-		"footer"=>false
-	]);
-
-	$page->setTpl("login");
-});
-
-$app->post('/admin/login', function() {
-	user::login($_POST["login"], $_POST["password"]);
-	header("Location: /admin");
-	exit;
-});
-
-$app->get('/admin/logout', function(){
-	User::logout();
-	header("Location: /admin/login");
-	exit;
-});
+	
 
 $app->run();
-
  ?>
